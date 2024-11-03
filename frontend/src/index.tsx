f17e4b983de9396e8efd2e10e347e3ab76247f7b
flyryan <flyryan@users.noreply.github.com>
2024-11-02 23:28:28 -0400

    Initial deployment to Sweden Central - Try 4

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ChatApp from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChatApp />
   </React.StrictMode>
 );
